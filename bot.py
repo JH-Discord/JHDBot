144919e98027dcbe87d4b431f2306cbfe2449673
Fumenoid <fumenoid@localhost.localdomain>
2020-04-22 03:41:24 +0530

    Few static commands added

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import json
 
 client = commands.Bot(command_prefix = '!')  #bot command prefix
 
-
+#EVENTS####################################################################################################
 #Event: when bot becomes ready.
 @client.event  #event/function decorators
 async def on_ready():
@@ -25,10 +25,13 @@ async def on_member_leave(member):  #a function which works when any member left
     channel = client.get_channel(id=698224498796920885)
     await channel.send(f'Ah Goodbye {member}, you won\'t be missed') #doesn't print it, idk why
 
+###############################################################################
+#Ping Command to check if server is up or not
 @client.command()       #creating Commands ctx is something like context, send automatically 
 async def ping(ctx):
     await ctx.send(f"Ping - {round(client.latency * 1000)}ms")
 
+#########################################TEMPSTUFFNEEDTOBEREMOVEDLATER##########
 @client.command(aliases=['tm']) #aliases - other name for function/command
 async def tellme(ctx, *, input):    # `*, input` takes all the input till the end of the line
     if(input=="Is Fume Awesome" or input=="is fume awesome"):
@@ -36,7 +39,37 @@ async def tellme(ctx, *, input):    # `*, input` takes all the input till the en
     else:
         await ctx.send(f"your input:{input} \nAnswer: NO") #replies No to everything user says ! 
 
+    
+##################################################################################
+#beginner command
+@client.command(aliases=['bgn'])       #creating Commands ctx is something like context, send automatically 
+async def beginner(ctx):
+    await ctx.send('I. Bandit OverTheWire: (https://overthewire.org/wargames/bandit/) A wargame focusing on basic Linux commands and privilege escalation. Questions can be asked in {#over-the-wire}\n\nII. PicoCTF: (https://picoctf.com/) A very beginner CTF that is up year round for practice. Questions can be asked in {#capture-the-flag} \n\nIII. Try Hack Me: (https://tryhackme.com/) A beginner friendly platform focusing on learning how to hack in more real world situations than CTFs. There are "rooms" for all levels and walkthroughs are available if necessary. Questions can be asked in {#tryhackme}\n\nIV. Natas OverTheWire: (https://overthewire.org/wargames/natas/) A wargame focusing on teaching the basics of server side web-security. Questions can also be asked in the {#over-the-wire} channel.\n\nFor any additional questions or concerns, please consult the {#beginners} channel')
 
+############################################################################
+#nypa command
+@client.command(aliases=['nypa'])       #creating Commands ctx is something like context, send automatically 
+async def notyourpersonalavengers(ctx):
+    await ctx.send(f"We aren't your personal Avengers. You are going to need to use other methods for that. Customer support and local law enforcement are probably best depending on the circumstance.")
+
+##################################################################################
+#blackhat command
+@client.command(aliases=['bt'])       #creating Commands ctx is something like context, send automatically 
+async def blackhat(ctx):
+    await ctx.send("Unfortunately it seems as though you are discussing blackhat activities. The term Blackhat refers to hacking for personal gain or to be generally malicious. Please refer to {#obligatory-rules} . We do not and cannot support illegal or immoral activities as mentioned above.")
+
+
+##################################################################################
+#account command
+@client.command()       #creating Commands ctx is something like context, send automatically 
+async def account(ctx):
+    await ctx.send('Sorry. We can\'t help with account recovery. Your only path for that is to contact support for the respective service. "I forgot my password" is often a good place to start.')
+
+##################################################################################
+#ctfwhat command
+@client.command()       #creating Commands ctx is something like context, send automatically 
+async def ctfwhat(ctx):
+    await ctx.send('CTF\'s are competitions you can participate into to practice and learn cybersecurity skills in a legal way. Here is a pretty cool video that explains them. https://www.youtube.com/watch?v=8ev9ZX9J45A&t=2s \n\nWargames are similar but run all the time where as CTFs tend to just run for a few days.')
 
 
 ################################################################################################################################################
---
+++ command_desc.md
@@ -0,0 +1,27 @@
+# Command List
+#####blackhat    
+Unfortunately it seems as though you are discussing blackhat activities. The term Blackhat refers to hacking for personal gain or to be generally malicious. Please refer to {#obligatory-rules} . We do not and cannot support illegal or immoral activities as mentioned above.
+
+#####nypa    
+We aren't your personal Avengers. You are going to need to use other methods for that. Customer support and local law enforcement are probably best depending on the circumstance. 
+
+#####beginner
+I. Bandit OverTheWire: (https://overthewire.org/wargames/bandit/) A wargame focusing on basic Linux commands and privilege escalation. Questions can be asked in {#over-the-wire}
+
+II. PicoCTF: (https://picoctf.com/) A very beginner CTF that is up year round for practice. Questions can be asked in {#capture-the-flag} 
+
+III. Try Hack Me: (https://tryhackme.com/) A beginner friendly platform focusing on learning how to hack in more real world situations than CTFs. There are "rooms" for all levels and walkthroughs are available if necessary. Questions can be asked in {#tryhackme}
+
+IV. Natas OverTheWire: (https://overthewire.org/wargames/natas/) A wargame focusing on teaching the basics of server side web-security. Questions can also be asked in the {#over-the-wire} channel.
+
+ For any additional questions or concerns, please consult the {#beginners} channel 
+
+#####account    
+Sorry. We can't help with account recovery. Your only path for that is to contact support for the respective service. "I forgot my password" is often a good place to start.
+
+#####ctfwhat
+CTF's are competitions you can participate into to practice and learn cybersecurity skills in a legal way. Here is a pretty cool video that explains them. https://www.youtube.com/watch?v=8ev9ZX9J45A&t=2s
+
+Wargames are similar but run all the time where as CTFs tend to just run for a few days.
+
+##Note, have to remove useless command.
