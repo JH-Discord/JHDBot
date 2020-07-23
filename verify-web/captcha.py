ee2f10e03678d8857fb456c7a0e98930ff312ca7
pop_eax <pop@PWNER>
2020-07-23 13:27:37 +0300

    started using env variables

--- verify-web/captcha.py
+++ verify-web/captcha.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm, RecaptchaField
 import requests
+import os
 
 class VerificationForm(FlaskForm):   
       recaptcha = RecaptchaField()
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'shhhhhhhhhhhhhhhhssh'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['RECAPTCHA_USE_SSL']= False
-app.config['RECAPTCHA_PUBLIC_KEY']='INSERT KEY HERE'
-app.config['RECAPTCHA_PRIVATE_KEY']='INSERT KEY HERE'
+app.config['RECAPTCHA_PUBLIC_KEY']= os.environ.get('RECAPTCHA_PUBLIC_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY']= os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
 
 @app.route('/verify', methods=['GET', 'POST'])
@@ -21,7 +22,7 @@ def verify():
         link = requests.get("http://localhost:3452").text
         return redirect(link)
 
-    return render_template("login.html", form=form)
+    return render_template("verify.html", form=form)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
