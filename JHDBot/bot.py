0629bb1a91f565c8fcfee37b76ec985e74399fb6
Python <salah.abbas142@gmail.com>
2020-07-28 22:36:26 -0500

    cleaned bot code for bot.py and general.py

--- JHDBot/bot.py
+++ JHDBot/bot.py
@@ -6,26 +6,29 @@ import sys
 import os
 import random
 import helpembed
-#import configfile
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix=str(os.environ.get('BOT_PREFIX_CHARACTER')), case_insensitive=True)  # bot command prefix
+bot = commands.Bot(
+    # bot command prefix
+    command_prefix=str(os.environ.get('BOT_PREFIX_CHARACTER')), 
+    case_insensitive=True
+)  
 bot.remove_command('help')
 # Loading Cogs
 
 extensions = ['moderation', 'veteran', 'general', 'verification']
 
 if __name__ == '__main__':
-    sys.path.insert(1, os.getcwd() + '/cogs/')
     for extension in extensions:
         try:
-            bot.load_extension(extension)
+            bot.load_extension(f"cogs.{extension}")
         except Exception as e:
             print(f'Failed to load cogs : {e}')
 
-
-# EVENTS
+# ==========================
+# ==========EVENTS==========
+# ==========================
 
 # Event: when bot becomes ready.
 @bot.event  # event/function decorators
@@ -38,13 +41,16 @@ async def on_ready():
 async def on_member_join(member):  # a function which works when any member joins,need param `member`
     print(f'{member} has joined the server :)')
     if str(member.name) == 'username123':
-        channel = discord.utils.get(member.guild.channels, name='moderators')
-        await channel.send(f'Warning : {member.mention} arrived in the server!')
-        channel2 = discord.utils.get(member.guild.channels, name="veteran-chat")
-        await channel2.send(f'Warning : {member.mention} arrived in the server!')
-    channel = discord.utils.get(member.guild.channels, name='welcome')
+
+        channels = ["moderators", "veteran-chat"]
+
+        [await discord.utils.get(member.guild.channels, name=channel)\
+            .send(f'Warning : {member.mention} arrived in the server!')\
+                for channel in channels]
+
+    welcome_channel = discord.utils.get(member.guild.channels, name='welcome')
     rules_channel = discord.utils.get(member.guild.channels, name='obligatory-rules')
-    await channel.send(
+    await welcome_channel.send(
         f'***Hi there, {member.mention} Welcome to JHDiscord!***\n\nTo gain access to the rest of the server. please '
         f'read the {rules_channel.mention} and then verify yourself.\nTo Verify yourself, Please use command '
         f'`$verify` and '
@@ -53,19 +59,24 @@ async def on_member_join(member):  # a function which works when any member join
         f'need to ping us but you can still tell us if you face a problem in this channel\n\nAlso the JHD_Bot will '
         f'send you a DM, so please make sure you have DM\'s from server members `on` in `privacy settings` before you '
         f'use `$verify` command, thanks')
+
     logchannel = discord.utils.get(member.guild.channels, name='join-leave')
+
     emb = discord.Embed(description=f'User - {member.mention}\nId - {member.id}\n', colour=0x3CFF4C)
     emb.set_author(name='Member Joined', icon_url=f"{member.avatar_url}")
     emb.set_footer(text=f'Join Log')
+
     await logchannel.send(embed=emb)
 
 #on member leave logs
 @bot.event
 async def on_member_remove(member):  #a function which works when any member lefts,need param `member`
     logchannel = discord.utils.get(member.guild.channels, name='join-leave')
+
     emb = discord.Embed(description=f'User - {member.mention}\nId - {member.id}\n', colour=0xFF693C)
     emb.set_author(name='Member Left', icon_url=f"{member.avatar_url}")
     emb.set_footer(text=f'Leave Log')
+    
     await logchannel.send(embed=emb)
 
 #Voice channel logs
@@ -124,10 +135,9 @@ async def on_message_delete(message):  # message deletion logs
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send('Invalid command. Please use `$help` to know list current valid commands.')
+        await ctx.send(f'Invalid command. Please use `{bot.command_prefix}help` to know list current valid commands.')
     else:
-        await ctx.send(
-            f'An error occurred. Please use `{bot.command_prefix}reportbot <Error>`')
+        await ctx.send(f'An error occurred. Please use `{bot.command_prefix}reportbot <Error>`')
 
 
 @bot.event
@@ -135,73 +145,57 @@ async def on_message(message):
     if 'https://' in message.content.lower() or 'http://' in message.content.lower() or 'ftp://' in message.content.lower():
         if str(message.channel) == "resources":
             with open('/home/ubuntu/JHD_Resources/botfile.md', 'a+') as fa:
-                fa.write("## " + str(message.author.name) + "\n")
-                fa.write("Message : " + str(message.content) + "\n\n")
+                fa.write(f"## {message.author.name}\n")
+                fa.write(f"Message : {message.content} \n\n")
                 fa.write("-----\n")
-    else:
-        await bot.process_commands(message)
-        return
     await bot.process_commands(message)
 
+# ==========================
+# =========COMMANDS=========
+# ==========================
 
 # JHDbot help message
 @bot.command(name="help")  # alias of command name
 async def _help(ctx, helprole=None):  # role-vise help section
-    role = discord.utils.get(ctx.author.roles, name='Veteran')
-    cool_people = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-    if (str(
-            ctx.message.channel) == 'bot-commands' or role is not None or cool_people is not None
-            or ctx.message.author.guild_permissions.manage_messages):
-        help_roles = ['Veteran', 'veteran', 'Moderator', 'moderator']
-        if helprole in help_roles:
-            if helprole == 'Veteran' or helprole == 'veteran':
-                emb = discord.Embed(description=helpembed.veteranhelplist, colour=0xff002a)
-            elif helprole == 'Moderator' or helprole == 'moderator':
-                emb = discord.Embed(description=helpembed.moderator_help_list, colour=0xff002a)
-            await attach_embed_info(ctx, emb)
-            await ctx.send(embed=emb)
+    if await isAuthorized(ctx):
+            
+        if helprole and helprole.lower() == 'veteran':
+            emb = discord.Embed(description=helpembed.veteranhelplist, colour=0xff002a)
+        elif helprole and helprole.lower() == 'moderator':
+            emb = discord.Embed(description=helpembed.moderator_help_list, colour=0xff002a)
         else:
             emb = discord.Embed(title='John Hammond Discord', url='https://www.youtube.com/user/RootOfTheNull',
-                                description=helpembed.memberhelplist, color=0xff002a)
-            await attach_embed_info(ctx, emb)
-            await ctx.send(embed=emb)
-    else:
-        await ctx.send('Please use this command in `#bot-commands`')
-
+                description=helpembed.memberhelplist, color=0xff002a)
+        
+        await attach_embed_info(ctx, emb)
+        await ctx.send(embed=emb)
 
 # FAQ message
 @bot.command(aliases=['qna'])
 async def faq(ctx):
-    role = discord.utils.get(ctx.author.roles, name='Veteran')
-    coolpeople = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-    if (str(
-            ctx.message.channel) == 'bot-commands' or role is not None or coolpeople is not None
-            or ctx.message.author.guild_permissions.manage_messages):
+    if await isAuthorized(ctx):
+
         emb = discord.Embed(description=helpembed.faq, colour=0xff002a)
         await attach_embed_info(ctx, emb)
         await ctx.send(embed=emb)
-    else:
-        await ctx.send('Please use this command in `#bot-commands`')
 
 
 # Channel desc message
 @bot.command(aliases=['chdesc', 'channeldesc'])
 async def channel_desc(ctx):
-    role = discord.utils.get(ctx.author.roles, name='Veteran')
-    coolpeople = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-    if (str(
-            ctx.message.channel) == 'bot-commands' or role is not None or coolpeople is not None
-            or ctx.message.author.guild_permissions.manage_messages):
+    if await isAuthorized(ctx):
 
         emb = discord.Embed(description=helpembed.channels, colour=0xff002a)
         await attach_embed_info(ctx, emb)
         await ctx.message.author.send(embed=emb)
+
         emb = discord.Embed(description=helpembed.channels2, colour=0xff002a)
         await attach_embed_info(ctx, emb)
         await ctx.message.author.send(embed=emb)
-    else:
-        await ctx.send('Please use this command in `#bot-commands`')
 
+# ==========================
+# ========FUNCTIONS=========
+# ==========================
 
 async def attach_embed_info(ctx=None, embed=None):
     embed.set_author(name='JHDiscord Bot', icon_url=f'{ctx.guild.icon_url}')
@@ -209,8 +203,22 @@ async def attach_embed_info(ctx=None, embed=None):
     embed.set_footer(text='by: JHD Moderation team ')
     return embed
 
+async def isAuthorized(ctx, veteran=True, moderator=True):
+    authorized_roles = []
+
+    if veteran:     authorized_roles.append("Veteran")
+    if moderator:   authorized_roles.append("Moderator Emeritus")
+
+    user_roles = [discord.utils.get(ctx.author.roles, name=role) for role in authorized_roles]
+    
+    if (str(ctx.message.channel) == 'bot-commands' or user_roles
+        or ctx.message.author.guild_permissions.manage_messages):
+        return True
+    
+    await ctx.send('Please use this command in `#bot-commands`')
+    return False
+
 # Token
 TOKEN = os.getenv("DISCORD_API_TOKEN")
 bot.run(TOKEN)  # token
 print("Bot started press ctrl+c to exit....")
-#bot.run(configfile.bot_token)
--- JHDBot/cogs/general.py
+++ JHDBot/cogs/general.py
@@ -12,80 +12,71 @@ class GeneralCog(commands.Cog):
     @commands.command()
     # creating Commands ctx is something like context, send automatically
     async def ping(self, ctx):
-        role = discord.utils.get(ctx.author.roles, name='Veteran')
-        cool_people = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-        if (str(
-                ctx.message.channel) == "bot-commands" or role is not None or cool_people is not None
-                or ctx.message.author.guild_permissions.manage_messages):
+        if await self.isAuthorized(ctx):
             await ctx.send(f'Ping! - {round(self.bot.latency * 1000)}ms')
-        else:
-            await ctx.send('Please use this command in `#bot-commands`')
 
     # For Likt
     @commands.command()
     async def solve(self, ctx, *, input=None):
-        role = discord.utils.get(ctx.author.roles, name='Veteran')
-        cool_people = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-        if (str(
-                ctx.message.channel) == "bot-commands" or role is not None or cool_people is not None
-                or ctx.message.author.guild_permissions.manage_messages):
+        if await self.isAuthorized(ctx):
             await ctx.send("That is a definite maybe")
-        else:
-            await ctx.send("Please use this command in `#bot-commands`")
 
     # Report bot command
     @commands.command(aliases=['reportbot'])
     async def report_bot(self, ctx, *, reason=None):
-        coolpeople = discord.utils.get(ctx.author.roles, name="Moderator Emeritus")
-        if (str(
-                ctx.message.channel) == "bot-commands" or coolpeople != None or ctx.message.author.guild_permissions.manage_messages):
-            if reason == None:
-                await ctx.send("Invalid syntax, please add the issue you are facing.")
+        if await self.isAuthorized(ctx, veteran=False):
+            if reason:
+                dev_channel = await discord.utils.get(ctx.message.author.guild.channels, name="dev-team")
+                await moderators_channel.send(f'Reported by user {ctx.message.author} : Complain against user {user} - {reason}')
+                await ctx.send("Your report has been successfully forwarded to the devs")
             else:
-                creator = await self.bot.fetch_user(554907015785218050)
-                await creator.send(f"Reported by user {ctx.message.author} : " + reason)
-                await ctx.send("Your report has been successfully forwarded to moderators")
-        else:
-            await ctx.send("Please use this command in `#bot-commands`")
+                await ctx.send("Invalid syntax, please add the issue you are facing.")
 
     # reporting users
     @commands.command()
     async def report(self, ctx, user=None, *, reason=None):
-        cool_people = discord.utils.get(ctx.author.roles, name="Moderator Emeritus")
-        if (str(
-                ctx.message.channel) == "bot-commands" or cool_people is not None or
-                ctx.message.author.guild_permissions.manage_messages):
-            if reason is None or user is None:
+        if await self.isAuthorized(ctx, veteran=False):
+            if reason and user:
+                moderators_channel = discord.utils.get(ctx.message.author.guild.channels, name="moderators")
+                await moderators_channel.send(f'Reported by user {ctx.message.author} : Complain against user {user} - {reason}')
+                await ctx.send('Your report has been successfully forwarded to moderators')
+            else:
                 await ctx.send(f'Invalid syntax, please check `{self.bot.command_prefix}help` to check the syntax and '
                                f'pass proper arguments.')
-            else:
-                channel = discord.utils.get(ctx.message.author.guild.channels, name="moderators")
-                await channel.send(f'Reported by user {ctx.message.author} : Complain against user {user} - ' + reason)
-                await ctx.send('Your report has been successfully forwarded to moderators')
-        else:
-            await ctx.send('Please use this command in `#bot-commands`')
+                
 
     # Suggestion command
     @commands.command()
     async def suggest(self, ctx, *, sug=None):
         cool_people = discord.utils.get(ctx.author.roles, name='Moderator Emeritus')
-        if (str(
-                ctx.message.channel) == "bot-commands" or cool_people is not None
-                or ctx.message.author.guild_permissions.manage_messages):
-            if sug is None:
-                await ctx.send("oops, seems like you forgot to add the suggestion .")
-            else:
-                channel = discord.utils.get(ctx.message.author.guild.channels, name="suggestions")
+        if await self.isAuthorized(ctx, veteran=False):
+            if sug:
+                suggestions_channel = discord.utils.get(ctx.message.author.guild.channels, name="suggestions")
                 emb = discord.Embed(description=sug, colour=0xff002a)
                 emb.set_author(name=f'{ctx.message.author}', icon_url=f"{ctx.message.author.avatar_url}")
                 emb.set_footer(text=f'Submit your suggestions using: '
                                     f'{self.bot.command_prefix}suggest <suggestion> in #bot-commands')
-                msg = await channel.send(embed=emb)
+                msg = await suggestions_channel.send(embed=emb)
                 await msg.add_reaction('👍')
                 await msg.add_reaction('👎')
-                await ctx.send(f'Your suggestion has been added in {channel.mention}')
-        else:
-            await ctx.send('Please use this command in `#bot-commands`')
+                await ctx.send(f'Your suggestion has been added in {suggestions_channel.mention}')
+            else:
+                await ctx.send("oops, seems like you forgot to add the suggestion .")
+    
+    async def isAuthorized(self, ctx, veteran=True, moderator=True):
+        authorized_roles = []
+
+        if veteran:     authorized_roles.append("Veteran")
+        if moderator:   authorized_roles.append("Moderator Emeritus")
+
+        user_roles = [discord.utils.get(ctx.author.roles, name=role) for role in authorized_roles]
+        
+        if (str(ctx.message.channel) == 'bot-commands' or user_roles
+            or ctx.message.author.guild_permissions.manage_messages):
+            return True
+        
+        await ctx.send('Please use this command in `#bot-commands`')
+        return False
 
 
 def setup(bot):
