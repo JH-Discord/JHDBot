835a121580afc09ff13dbaa8f562e9afd89e528d
fumenoid <fumenoid@localhost.localdomain>
2020-09-25 15:28:04 +0530

    requirements added

--- JHDBot/cogs/veteran.py
+++ JHDBot/cogs/veteran.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
 import asyncio
-import urllib
-
+import urllib.request
+from requests_html import HTML, HTMLSession
+import requests
+import re
 
 class VeteranCog(commands.Cog):
 
