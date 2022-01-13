c81821ebab5dcbec836c2d29a0b851b4f1f5d93d
moknah <actuallymoknah@gmail.com>
2022-01-13 13:25:45 -0100

    Enhances block mode encryption of VLAN sock routers for ERP BI analysis

--- JHDBot/cogs/strings.py
+++ JHDBot/cogs/strings.py
@@ -2,26 +2,26 @@ faq = """
 ***FREQUENTLY ASKED QUESTIONS***
 
 **Can I ping or dm John ?**
-Yes, feel free to ping or dm john, his response time depends on how busy he is, but he is a teddy bear, so you will get a response sooner or later
+Yes, feel free to ping or dm John. His response time depends on how busy he is, but he is a teddy bear, so you will get a response sooner or later.
 
 **Can I ping moderators role/admins ?**
-Yes, but when it is really urgent, otherwise just ping a moderator or admin who is online or active in chat at that time.
+Yes, but only when it is really urgent, otherwise just ping a moderator or admin who is online or active in chat at that time.
 
 **From where can I get a list of ctf's ?**
-Use the website `ctftime.org`, or use the `NullCTF bot`
+Use the website `ctftime.org`, or use the `NullCTF bot`.
 
 **Can you please help me get my (INSTA|FB|STEAM...) account back ?**
-NO, reach the support of respective service/social network
+NO, PLEASE reach out to support of the respective service/social network instead. They're getting paid for it.
 
 **Can I send a DM to anyone in the server ?**
-No, please ask that person if he/she is okay with DM's, before sending a DM
+No, please ask that person if they're okay with DMs before sending a DM out of nowhere.
 
 **Is there place where JHD resources are stored ?**
-We have a repo in which the JHD_Bot dump all the resources from `#resources` channel.
+We have a repo into which the JHD_Bot dumps all the resources from `#resources` channel.
 link - https://github.com/Fumenoid/JHD_Resources
 
 **Is Shiptoasting channel a NSFW channel ?**
-NO !! That channel is only for tech memes and SFW image posting, we have marked that channel NSFW because some meme contains explicit language.
+NO !! That channel is only for tech memes and SFW image posting, we have marked that channel NSFW because memes tend to contain explicit language.
 
 **How did you guys build the JHD_bot ? resources ?**
 Really thankfull for a guy named `jared`, his youtube tutorials & Discord, `Drillenissen` and `tsu_katana` too, it's a indirect shoutout to them, they don't know about it : P
@@ -80,7 +80,7 @@ A channel for discussing anything CTF that doesn't have a specific channel. PLEA
 Channel for OverTheWire challenges. Please keep spoilers in DMs
 
 `#tryhackme`
-Channel for discussion of Try Hack Me http://tryhackme.com
+Channel for discussion of TryHackMe, http://tryhackme.com
 
 `#hackthebox`
 Request for help in DM, asking questions for HackTheBox, please do not post spoilers
@@ -96,7 +96,7 @@ Challenges to practise your pwn skills on
 
 **Hacking/Security**
 `#beginners`
-Channel for those new into hacking/security. Please be respectful with your questions/responses and DO NOT put down others for not having a level of knowledge similar to yours. This  is a NEWB FRIENDLY ZONE
+Channel for those new into hacking/security. Welcome! Please be respectful with your questions/responses and DO NOT put down others for not having a level of knowledge similar to yours. This is a NEWB FRIENDLY ZONE
 
 `#news`
 Feel free to link news stories relevant to hacking, cybersecurity, ctfs, tech, etc. here. 
@@ -105,16 +105,16 @@ Feel free to link news stories relevant to hacking, cybersecurity, ctfs, tech, e
 Browsers, web apps, and all that jazz.
 
 `#osint-privacy`
-Open Source Intelligent and privacy discussion and so forth. Do NOT dox anyone!
+Open Source Intelligence and privacy discussion and so forth. Do NOT doxx anyone!
 
 `#pwning-and-reversing`
 Discussion that relates to binary exploitation and reverse engineering.
 
 `#blue-ir-forensics`
-Blue teaming, incident response and forensics discussion all goes here.
+Blue teaming, incident response, and forensics discussion all goes here.
 
 `#wireless-hacking`
-Discussion about Wifi, RFID, forms of wireless hacking.
+Discussion about Wi-Fi, RFID, forms of wireless hacking.
 
 `#cryptography`
 Discussion of crypto and associated maths-y stuff.
@@ -123,20 +123,20 @@ Discussion of crypto and associated maths-y stuff.
 Meddling with phones, tablets and portable decvices can go here
 
 `#hardware-hacking`
-Stuff about attacking vulnerable physical hardware can go here
+Stuff about attacking and defending vulnerable physical hardware can go here
 
 **Development**
 `#programming`
-Channel for anything programming related questions. 
+Channel for anything programming related. 
 
 `#webdev`
-Channel for anything webdev related questions. 
+Channel for anything webdev related. 
 
 `#certifications`
-Discuss the certifications you have or want here. What they are. Which ones are useful. How to get them. etc. Do NOT ask for copyrighted content. Obtain it from legal sources.
+Discuss the certifications you have or want here. What they are. Which ones are useful. How to get them. etc. Do NOT ask for copyrighted content. Obtain it from legal sources. Make sure to check pins.
 
 `#katana-pwncat`
-Discuss and coordinate work on Katana Modules/Pwncat here.
+Discuss and coordinate work on Katana modules/Pwncat here.
 """
 
 url = """
